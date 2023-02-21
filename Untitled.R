c1da9d41d791299342d1d634db9d788169f1e6a0
Maxime Borel <58341766+maxrel95@users.noreply.github.com>
2023-02-22 00:46:37 +0100

    regression in r

--- Untitled.R
+++ Untitled.R
@@ -15,10 +15,26 @@ library( stargazer )
 require(lmtest)
 
 
-dataTable = as.data.frame( fread( "test.csv" ) ) # import data
-dataTable$er = dataTable$er
+dataTable = as.data.frame( fread( "Data/benchmark2.csv" ) ) # import data
+dataTable$er = dataTable$er*100
 fpmg <- pmg(er~  1 +GP + logbm + logme + reversal + mom, dataTable, index=c("jdate","permno"))
 summary( fpmg )
+
+dataTable$residff3 = dataTable$residff3*100
+fpmg2 <- pmg(residff3~  1 +GP + logbm + logme + reversal + mom, dataTable, index=c("jdate","permno"))
+summary( fpmg2 )
+
+dataTable$residff6 = dataTable$residff6*100
+fpmg3 <- pmg(residff6~  1 +GP + logbm + logme + reversal + mom, dataTable, index=c("jdate","permno"))
+summary( fpmg3 )
+
+m <- floor(0.75 * nrow(dataTable)^(1/3))
+NW_VCOV <- NeweyWest(pmg(er~  1 +GP + logbm + logme + reversal + mom, dataTable, index=c("jdate","permno")), 
+                     lag = m - 1, prewhite = F, 
+                     adjust = T)
+coeftest(example_mod, vcov = NW_VCOV)
+
+
 stargazer( fpmg, out = 'testss.tex', digits = 3 )
 
 
