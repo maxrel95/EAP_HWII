ca16d14219710ff39a9e0cbbe4c5a078de2939ec
Maxime Borel <58341766+maxrel95@users.noreply.github.com>
2023-02-23 21:48:14 +0100

    comp w and wo micro fm interaction

--- Regressions.py
+++ Regressions.py
@@ -33,32 +33,32 @@ sic_df = all_df[ ( ( all_df['siccd'] < 9000 ) | ( all_df['siccd'] > 9799 ) ) &
                                ( ( all_df['siccd'] < 6000 ) | ( all_df['siccd'] > 6999 ) ) ]
 
 fm_a = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=all_df)
-res_a = fm_a.fit(cov_type='kernel')
-print(res_a.summary)
+res_a = fm_a.fit( cov_type='kernel' )
+print( res_a.summary )
 
 fm_ff3 = FamaMacBeth.from_formula( "residff3 ~ 1 + GP+gat+logbm+logme+reversal+mom", data=all_df)
 res_ff3 = fm_ff3.fit(cov_type='kernel')
-print(res_ff3.summary)
+print( res_ff3.summary )
 
 fm_ff6 = FamaMacBeth.from_formula( "residff6 ~ 1 + GP+gat+logbm+logme+reversal+mom", data=all_df)
 res_ff6 = fm_ff6.fit(cov_type='kernel')
-print(res_ff6.summary)
+print( res_ff6.summary )
 
 fm_l = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=large_df)
 res_l = fm_l.fit(cov_type='kernel')
-print(res_l.summary)
+print( res_l.summary )
 
 fm_s = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=small_df)
 res_s = fm_s.fit(cov_type='kernel')
-print(res_s.summary)
+print( res_s.summary )
 
 fm_m = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=micro_df)
 res_m = fm_m.fit(cov_type='kernel')
-print(res_m.summary)
+print( res_m.summary )
 
 fm_ind = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=sic_df)
 res_ind = fm_ind.fit(cov_type='kernel')
-print(res_ind.summary)
+print( res_ind.summary )
 
 df_q = pd.read_csv( "Data/all_df_q.csv" )
 df_q['jdate'] = pd.to_datetime( df_q['jdate'] )
@@ -71,7 +71,7 @@ res_q = fm_q.fit(cov_type='kernel')
 print(res_q.summary)
 
 # interactions
-interaction_df = all_df[ (all_df['szport']!='Micro') & ( ( all_df['siccd'] < 6000 ) | ( all_df['siccd'] > 6999 ) )]
+interaction_df = all_df
 
 fm_mom = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + mom*logbm", data=interaction_df)
 res_mom = fm_mom.fit(cov_type='kernel')
@@ -84,3 +84,18 @@ print(res_bm.summary)
 fm_int = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + logbm*mom + logbm*logme", data=interaction_df)
 res_int = fm_int.fit(cov_type='kernel')
 print(res_int.summary)
+
+interaction_df = all_df[ (all_df['szport']!='Micro') ]
+
+fm_mom_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + mom*logbm", data=interaction_df)
+res_mom_nomic = fm_mom_nomic.fit(cov_type='kernel')
+print(res_mom_nomic.summary)
+
+fm_bm_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom+ logbm*mom + logbm*logme", data=interaction_df)
+res_bm_nomic = fm_bm_nomic.fit(cov_type='kernel')
+print(res_bm_nomic.summary)
+
+fm_int_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + logbm*mom + logbm*logme", data=interaction_df)
+res_int_nomic = fm_int_nomic.fit(cov_type='kernel')
+print(res_int_nomic.summary)
+
