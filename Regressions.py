6cd73d6328b55755029f546ed5c5961623dc26fb
Maxime Borel <58341766+maxrel95@users.noreply.github.com>
2023-02-27 18:24:38 +0100

    compute own r2

--- Regressions.py
+++ Regressions.py
@@ -1,20 +1,42 @@
 from linearmodels import FamaMacBeth
 import pandas as pd
+import numpy as np
 
 
 data = pd.read_csv('Data/benchmark.csv')
 data['jdate'] = pd.to_datetime( data['jdate'] )
 pd.to_datetime( data['jdate'].unique()).sort_values()
 data.set_index( [ 'permno', 'jdate' ], inplace=True )
-data[ 'er' ] = data[ 'er' ]*100
+data[ 'er' ] = data[ 'er' ] *100
 
+import statsmodels.api as sm
 
-fm = FamaMacBeth.from_formula( "er ~ 1 + GP+logbm+logme+reversal+mom", data=data)
-res = fm.fit(cov_type='kernel')
+def fm( df, var, type='er' ):
+    x = df[var]
+    x = sm.add_constant( x )
+    y = df[type]
+    mdl = sm.OLS( y,x )
+    res = mdl.fit()
+    r2 = res.rsquared_adj
+    return r2
+
+import statsmodels.formula.api as smf
+
+def fm_iteraction( df, formula ):
+    mdl = smf.ols( formula=formula, data=df )
+    res = mdl.fit()
+    return res.rsquared_adj
+
+test = data.groupby('jdate').apply(fm, var=['GP','logbm','logme','reversal','mom'])
+
+fm_b = FamaMacBeth.from_formula( "er ~ 1 + GP+logbm+logme+reversal+mom", data=data)
+res = fm_b.fit(cov_type='kernel')
+r2_fm = data.groupby('jdate').apply(fm, var=['GP','logbm','logme','reversal','mom']).mean()
 print(res.summary)
 
 fm2 = FamaMacBeth.from_formula( "er ~ 1 +logbm+logme+reversal+mom", data=data)
 res2 = fm2.fit(cov_type='kernel')
+r2_fm2 = data.groupby('jdate').apply(fm, var=['logbm','logme','reversal','mom']).mean()
 print(res2.summary)
 
 all_df = pd.read_csv( 'Data/all_df.csv' )
@@ -34,30 +56,38 @@ sic_df = all_df[ ( ( all_df['siccd'] < 9000 ) | ( all_df['siccd'] > 9799 ) ) &
 
 fm_a = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=all_df)
 res_a = fm_a.fit( cov_type='kernel' )
+r2_fm_a = all_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
+
 print( res_a.summary )
 
 fm_ff3 = FamaMacBeth.from_formula( "residff3 ~ 1 + GP+gat+logbm+logme+reversal+mom", data=all_df)
 res_ff3 = fm_ff3.fit(cov_type='kernel')
+r2_fm_ff3 = all_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom'], type='residff3').mean()
 print( res_ff3.summary )
 
 fm_ff6 = FamaMacBeth.from_formula( "residff6 ~ 1 + GP+gat+logbm+logme+reversal+mom", data=all_df)
 res_ff6 = fm_ff6.fit(cov_type='kernel')
+r2_fm_ff6 = all_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom'], type='residff6').mean()
 print( res_ff6.summary )
 
 fm_l = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=large_df)
 res_l = fm_l.fit(cov_type='kernel')
+r2_fm_l = large_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
 print( res_l.summary )
 
 fm_s = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=small_df)
 res_s = fm_s.fit(cov_type='kernel')
+r2_fm_s = small_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
 print( res_s.summary )
 
 fm_m = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=micro_df)
 res_m = fm_m.fit(cov_type='kernel')
+r2_fm_m = micro_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
 print( res_m.summary )
 
 fm_ind = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=sic_df)
 res_ind = fm_ind.fit(cov_type='kernel')
+r2_fm_ind = sic_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
 print( res_ind.summary )
 
 df_q = pd.read_csv( "Data/all_df_q.csv" )
@@ -68,6 +98,7 @@ df_q[ 'er' ] = df_q[ 'er' ]*100
 
 fm_q = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=df_q)
 res_q = fm_q.fit(cov_type='kernel')
+r2_fm_q = df_q.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
 print(res_q.summary)
 
 # interactions
@@ -75,27 +106,37 @@ interaction_df = all_df
 
 fm_mom = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + mom*logbm", data=interaction_df)
 res_mom = fm_mom.fit(cov_type='kernel')
+r2_fm_mom = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + mom*logbm").mean()
 print(res_mom.summary)
 
 fm_bm = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom+ logbm*mom + logbm*logme", data=interaction_df)
 res_bm = fm_bm.fit(cov_type='kernel')
+r2_fm_bm = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom+ logbm*mom + logbm*logme").mean()
 print(res_bm.summary)
 
 fm_int = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + logbm*mom + logbm*logme", data=interaction_df)
 res_int = fm_int.fit(cov_type='kernel')
+r2_fm_int = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + logbm*mom + logbm*logme").mean()
 print(res_int.summary)
 
 interaction_df = all_df[ (all_df['szport']!='Micro') ]
 
 fm_mom_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + mom*logbm", data=interaction_df)
 res_mom_nomic = fm_mom_nomic.fit(cov_type='kernel')
+r2_fm_mom_nomic = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + mom*logbm").mean()
 print(res_mom_nomic.summary)
 
 fm_bm_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom+ logbm*mom + logbm*logme", data=interaction_df)
 res_bm_nomic = fm_bm_nomic.fit(cov_type='kernel')
+r2_fm_bm_nomic = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom+ logbm*mom + logbm*logme").mean()
 print(res_bm_nomic.summary)
 
 fm_int_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + logbm*mom + logbm*logme", data=interaction_df)
 res_int_nomic = fm_int_nomic.fit(cov_type='kernel')
+r2_fm_int_nomic = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + logbm*mom + logbm*logme").mean()
 print(res_int_nomic.summary)
+
+stats_all = all_df[ ['GP', 'gat', 'logbm', 'logme', 'reversal', 'mom']].describe( 
+    percentiles=[ .01, .25, .5, .75, .99 ]).T.round( 3 ).drop('count', axis=1)
+stats_all.round(2).to_latex( 'results/statistics.tex' )
 
