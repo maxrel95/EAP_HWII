9d6bd05a9d8e5553d08a4efb6a34308aeb75fd0c
Maxime Borel <58341766+maxrel95@users.noreply.github.com>
2023-02-28 11:13:06 +0100

    six month lag me in reg py and R

--- FMRegressions.R
+++ FMRegressions.R
@@ -12,10 +12,11 @@ library(sandwich)
 
 dataTable = as.data.frame( fread( "Data/benchmark.csv" ) ) # import data
 dataTable$er = dataTable$er*100
-fpmg <- pmg(er ~  1 +GP + logbm + logme + reversal + mom, dataTable, index=c("jdate","permno") )
-fpmg1 <- pmg(er ~  1  + logbm + logme + reversal + mom, dataTable, index=c("jdate","permno"), )
+fpmg <- pmg(er ~  1 +GP + logbm + logme1 + reversal + mom, dataTable, index=c("jdate","permno"), model = "dmg" )
+fpmg1 <- pmg(er ~  1  + logbm + logme1 + reversal + mom, dataTable, index=c("jdate","permno") )
 
 summary( fpmg )
+summary( fpmg1 )
 
 stargazer( fpmg1,fpmg, out = 'results/fmregression_replication.tex', digits = 2,
            title="Regression Results", digits.extra = 1,
@@ -25,47 +26,47 @@ stargazer( fpmg1,fpmg, out = 'results/fmregression_replication.tex', digits = 2,
 # Full annually with excess return 
 df_a_full = as.data.frame( fread( "Data/all_df.csv" ) )
 df_a_full$er = df_a_full$er*100
-fm_a_full_er <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom, df_a_full, index=c("jdate","permno") )
+fm_a_full_er <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom, df_a_full, index=c("jdate","permno") )
 summary( fm_a_full_er )
 
 # same basis as quarterly 
 df_a_full_filtered = df_a_full %>%
   filter( jdate >='1975-07-30' )
-fm_a_full_filtered <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom, df_a_full_filtered, index=c("jdate","permno") )
+fm_a_full_filtered <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom, df_a_full_filtered, index=c("jdate","permno") )
 summary( fm_a_full_filtered )
 
 # Full annually with ff3
 df_a_full$residff3 = df_a_full$residff3*100
-fm_a_full_ff3 <- pmg(residff3 ~ 1 + GP + gat + logbm + logme + reversal + mom, df_a_full, index=c("jdate","permno") )
+fm_a_full_ff3 <- pmg(residff3 ~ 1 + GP + gat + logbm + logme1 + reversal + mom, df_a_full, index=c("jdate","permno") )
 summary( fm_a_full_ff3 )
 
 # Full annually with ff3
 df_a_full$residff6 = df_a_full$residff6*100
-fm_a_full_ff6 <- pmg(residff6 ~ 1 + GP + gat + logbm + logme + reversal + mom, df_a_full, index=c("jdate","permno") )
+fm_a_full_ff6 <- pmg(residff6 ~ 1 + GP + gat + logbm + logme1 + reversal + mom, df_a_full, index=c("jdate","permno") )
 summary( fm_a_full_ff6 )
 
 # Large annually with er
 df_a_large = df_a_full %>% 
   filter( szport == 'Large' )
-fm_a_large_er <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom, df_a_large, index=c("jdate","permno") )
+fm_a_large_er <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom, df_a_large, index=c("jdate","permno") )
 summary( fm_a_large_er )
 
 # Small annually with er
 df_a_small = df_a_full %>% 
   filter( szport == 'Small' )
-fm_a_small_er <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom, df_a_small, index=c("jdate","permno") )
+fm_a_small_er <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom, df_a_small, index=c("jdate","permno") )
 summary( fm_a_small_er )
 
 # Micro annually with er
 df_a_micro = df_a_full %>% 
   filter( szport == 'Micro' )
-fm_a_micro_er <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom, df_a_micro, index=c("jdate","permno") )
+fm_a_micro_er <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom, df_a_micro, index=c("jdate","permno") )
 summary( fm_a_micro_er )
 
 # Exclude Fin/Util annually with er
 df_a_indu = df_a_full %>% 
   filter( ( siccd < 6000 | siccd > 6799 ) & ( siccd < 9000 | siccd > 9799 ) )
-fm_a_indu_er <- pmg(er ~1 + GP + gat + logbm + logme + reversal + mom, df_a_indu, index=c("jdate","permno") )
+fm_a_indu_er <- pmg(er ~1 + GP + gat + logbm + logme1 + reversal + mom, df_a_indu, index=c("jdate","permno") )
 summary( fm_a_indu_er )
 
 # Full quarterly
@@ -79,19 +80,19 @@ nbrOfObsPerYear = df_q_full %>%
 df_q_full_filtered = df_q_full %>%
   filter( jdate >='1975-07-30' )
 df_q_full_filtered$er = df_q_full_filtered$er*100
-fm_q_full <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom, df_q_full_filtered, index=c("jdate","permno") )
+fm_q_full <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom, df_q_full_filtered, index=c("jdate","permno") )
 summary( fm_q_full )
 
 # not reported but they dont give more value to display them 
 # Full quarterly  with ff3 
 df_q_full_filtered$residff6 = df_q_full_filtered$residff6*100
-fm_q_full_ff6 <- pmg(residff6 ~ 1 + GP + gat + logbm + logme + reversal + mom, df_q_full_filtered, index=c("jdate","permno") )
+fm_q_full_ff6 <- pmg(residff6 ~ 1 + GP + gat + logbm + logme1 + reversal + mom, df_q_full_filtered, index=c("jdate","permno") )
 summary( fm_q_full_ff6 )
 
 # quarterly without financial and utilities 
 df_q_indu = df_q_full_filtered %>% 
   filter( ( siccd < 6000 | siccd > 6799 ) & ( siccd < 9000 | siccd > 9799 ) )
-fm_q_indu_er <- pmg(er ~1 + GP + gat + logbm + logme + reversal + mom, df_q_indu, index=c("jdate","permno") )
+fm_q_indu_er <- pmg(er ~1 + GP + gat + logbm + logme1 + reversal + mom, df_q_indu, index=c("jdate","permno") )
 summary( fm_q_indu_er )
 #trace(stargazer:::.stargazer.wrap, edit = T)
 
@@ -119,52 +120,52 @@ stargazer( fm_a_full_er, fm_a_large_er, fm_a_small_er, fm_a_micro_er, type = 'te
 interaction_df = df_a_full %>%
   filter( szport !='Micro' )
 
-gp_gat <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + GP*gat, interaction_df, index=c("jdate","permno") )
-gp_logbm <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + GP*logbm, interaction_df, index=c("jdate","permno") )
-gp_logme <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + GP*logme, interaction_df, index=c("jdate","permno") )
-gp_reversal <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + GP*reversal, interaction_df, index=c("jdate","permno") )
-gp_mom <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + GP*mom, interaction_df, index=c("jdate","permno") )
+gp_gat <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + GP*gat, interaction_df, index=c("jdate","permno") )
+gp_logbm <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + GP*logbm, interaction_df, index=c("jdate","permno") )
+gp_logme1 <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + GP*logme1, interaction_df, index=c("jdate","permno") )
+gp_reversal <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + GP*reversal, interaction_df, index=c("jdate","permno") )
+gp_mom <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + GP*mom, interaction_df, index=c("jdate","permno") )
 
-gat_logbm <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + gat*logbm, interaction_df, index=c("jdate","permno") )
-gat_logme <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + gat*logme, interaction_df, index=c("jdate","permno") )
-gat_reversal <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + gat*reversal, interaction_df, index=c("jdate","permno") )
-gat_mom <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + gat*mom, interaction_df, index=c("jdate","permno") )
+gat_logbm <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + gat*logbm, interaction_df, index=c("jdate","permno") )
+gat_logme1 <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + gat*logme1, interaction_df, index=c("jdate","permno") )
+gat_reversal <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + gat*reversal, interaction_df, index=c("jdate","permno") )
+gat_mom <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + gat*mom, interaction_df, index=c("jdate","permno") )
 
-logbm_logme <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + logbm*logme, interaction_df, index=c("jdate","permno") )
-logbm_reversal <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + logbm*reversal, interaction_df, index=c("jdate","permno") )
-logbm_mom <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + logbm*mom, interaction_df, index=c("jdate","permno") )
+logbm_logme1 <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + logbm*logme1, interaction_df, index=c("jdate","permno") )
+logbm_reversal <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + logbm*reversal, interaction_df, index=c("jdate","permno") )
+logbm_mom <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + logbm*mom, interaction_df, index=c("jdate","permno") )
 
-logme_reversal <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + logme*reversal, interaction_df, index=c("jdate","permno") )
-logme_mom <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + logme*mom, interaction_df, index=c("jdate","permno") )
+logme1_reversal <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + logme1*reversal, interaction_df, index=c("jdate","permno") )
+logme1_mom <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + logme1*mom, interaction_df, index=c("jdate","permno") )
 
-reversal_mom <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + reversal*mom, interaction_df, index=c("jdate","permno") )
+reversal_mom <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + reversal*mom, interaction_df, index=c("jdate","permno") )
 
 tstat_int = c(tail(gp_gat$coefficients,1)/sqrt(tail(diag(gp_gat$vcov),1)), tail(gp_logbm$coefficients,1)/sqrt(tail(diag(gp_logbm$vcov),1)),
-  tail(gp_logme$coefficients,1)/sqrt(tail(diag(gp_logme$vcov),1)), tail(gp_reversal$coefficients,1)/sqrt(tail(diag(gp_reversal$vcov),1)),
+  tail(gp_logme1$coefficients,1)/sqrt(tail(diag(gp_logme1$vcov),1)), tail(gp_reversal$coefficients,1)/sqrt(tail(diag(gp_reversal$vcov),1)),
   tail(gp_mom$coefficients,1)/sqrt(tail(diag(gp_mom$vcov),1)), tail(gat_logbm$coefficients,1)/sqrt(tail(diag(gat_logbm$vcov),1)),
-  tail(gat_logme$coefficients,1)/sqrt(tail(diag(gat_logme$vcov),1)), tail(gat_reversal$coefficients,1)/sqrt(tail(diag(gat_reversal$vcov),1)), 
-  tail(gat_mom$coefficients,1)/sqrt(tail(diag(gat_mom$vcov),1)), tail(logbm_logme$coefficients,1)/sqrt(tail(diag(logbm_logme$vcov),1)),
+  tail(gat_logme1$coefficients,1)/sqrt(tail(diag(gat_logme1$vcov),1)), tail(gat_reversal$coefficients,1)/sqrt(tail(diag(gat_reversal$vcov),1)), 
+  tail(gat_mom$coefficients,1)/sqrt(tail(diag(gat_mom$vcov),1)), tail(logbm_logme1$coefficients,1)/sqrt(tail(diag(logbm_logme1$vcov),1)),
   tail(logbm_reversal$coefficients,1)/sqrt(tail(diag(logbm_reversal$vcov),1)), tail(logbm_mom$coefficients,1)/sqrt(tail(diag(logbm_mom$vcov),1)), 
-  tail(logme_reversal$coefficients,1)/sqrt(tail(diag(logme_reversal$vcov),1)), tail(logme_mom$coefficients,1)/sqrt(tail(diag(logme_mom$vcov),1)),
+  tail(logme1_reversal$coefficients,1)/sqrt(tail(diag(logme1_reversal$vcov),1)), tail(logme1_mom$coefficients,1)/sqrt(tail(diag(logme1_mom$vcov),1)),
   tail(reversal_mom$coefficients,1)/sqrt(tail(diag(reversal_mom$vcov),1)) )
 tstat_int[order(abs(tstat_int), decreasing=TRUE)[1:5]]
 
-mom_interaction <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + reversal*mom + mom*logbm,
+mom_interaction <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + reversal*mom + mom*logbm,
                        df_a_full, index=c("jdate","permno") )
-rev_interaction <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + reversal*mom + reversal*gat,
+rev_interaction <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + reversal*mom + reversal*gat,
                        df_a_full, index=c("jdate","permno") )
-bm_interaction <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + logbm*mom + logbm*logme,
+bm_interaction <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + logbm*mom + logbm*logme1,
                       df_a_full, index=c("jdate","permno") )
-all_interaction = pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + reversal*mom + mom*logbm + logbm*logme,
+all_interaction = pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + reversal*mom + mom*logbm + logbm*logme1,
                       df_a_full, index=c("jdate","permno") )
 
-mom_interaction_nomic <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + reversal*mom + mom*logbm,
+mom_interaction_nomic <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + reversal*mom + mom*logbm,
                              interaction_df, index=c("jdate","permno") )
-rev_interaction_nomic <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + reversal*mom + reversal*gat,
+rev_interaction_nomic <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + reversal*mom + reversal*gat,
                              interaction_df, index=c("jdate","permno") )
-bm_interaction_nomic <- pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + logbm*mom + logbm*logme,
+bm_interaction_nomic <- pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + logbm*mom + logbm*logme1,
                             interaction_df, index=c("jdate","permno") )
-all_interaction_nomic = pmg(er ~ 1 + GP + gat + logbm + logme + reversal + mom + reversal*mom + mom*logbm + logbm*logme,
+all_interaction_nomic = pmg(er ~ 1 + GP + gat + logbm + logme1 + reversal + mom + reversal*mom + mom*logbm + logbm*logme1,
                             interaction_df, index=c("jdate","permno") )
 
 stargazer( mom_interaction, bm_interaction, all_interaction,  type = 'text', digits = 2,
--- Regressions.py
+++ Regressions.py
@@ -12,14 +12,14 @@ data.set_index( [ 'permno', 'jdate' ], inplace=True )
 data[ 'er' ] = data[ 'er' ] *100
 
 
-fm_b = FamaMacBeth.from_formula( "er ~ 1 + GP+logbm+logme+reversal+mom", data=data)
+fm_b = FamaMacBeth.from_formula( "er ~ 1 + GP+logbm+logme1+reversal+mom", data=data)
 res = fm_b.fit(cov_type='kernel')
-r2_fm = data.groupby('jdate').apply(fm, var=['GP','logbm','logme','reversal','mom']).mean()
+r2_fm = data.groupby('jdate').apply(fm, var=['GP','logbm','logme1','reversal','mom']).mean()
 print(res.summary)
 
-fm2 = FamaMacBeth.from_formula( "er ~ 1 + logbm + logme + reversal + mom", data=data)
+fm2 = FamaMacBeth.from_formula( "er ~ 1 + logbm + logme1 + reversal + mom", data=data)
 res2 = fm2.fit(cov_type='kernel')
-r2_fm2 = data.groupby('jdate').apply(fm, var=['logbm','logme','reversal','mom']).mean()
+r2_fm2 = data.groupby('jdate').apply(fm, var=['logbm','logme1','reversal','mom']).mean()
 print(res2.summary)
 
 all_df = pd.read_csv( 'Data/all_df.csv' )
@@ -37,39 +37,39 @@ micro_df = all_df[ all_df['szport']=='Micro']
 sic_df = all_df[ ( ( all_df['siccd'] < 9000 ) | ( all_df['siccd'] > 9799 ) ) &
                                ( ( all_df['siccd'] < 6000 ) | ( all_df['siccd'] > 6999 ) ) ]
 
-fm_a = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=all_df)
+fm_a = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom", data=all_df)
 res_a = fm_a.fit( cov_type='kernel' )
-r2_fm_a = all_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
+r2_fm_a = all_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme1','reversal','mom']).mean()
 print( res_a.summary )
 
-fm_ff3 = FamaMacBeth.from_formula( "residff3 ~ 1 + GP+gat+logbm+logme+reversal+mom", data=all_df)
+fm_ff3 = FamaMacBeth.from_formula( "residff3 ~ 1 + GP+gat+logbm+logme1+reversal+mom", data=all_df)
 res_ff3 = fm_ff3.fit(cov_type='kernel')
-r2_fm_ff3 = all_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom'], type='residff3').mean()
+r2_fm_ff3 = all_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme1','reversal','mom'], type='residff3').mean()
 print( res_ff3.summary )
 
-fm_ff6 = FamaMacBeth.from_formula( "residff6 ~ 1 + GP+gat+logbm+logme+reversal+mom", data=all_df)
+fm_ff6 = FamaMacBeth.from_formula( "residff6 ~ 1 + GP+gat+logbm+logme1+reversal+mom", data=all_df)
 res_ff6 = fm_ff6.fit(cov_type='kernel')
-r2_fm_ff6 = all_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom'], type='residff6').mean()
+r2_fm_ff6 = all_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme1','reversal','mom'], type='residff6').mean()
 print( res_ff6.summary )
 
-fm_l = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=large_df)
+fm_l = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom", data=large_df)
 res_l = fm_l.fit(cov_type='kernel')
-r2_fm_l = large_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
+r2_fm_l = large_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme1','reversal','mom']).mean()
 print( res_l.summary )
 
-fm_s = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=small_df)
+fm_s = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom", data=small_df)
 res_s = fm_s.fit(cov_type='kernel')
-r2_fm_s = small_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
+r2_fm_s = small_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme1','reversal','mom']).mean()
 print( res_s.summary )
 
-fm_m = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=micro_df)
+fm_m = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom", data=micro_df)
 res_m = fm_m.fit(cov_type='kernel')
-r2_fm_m = micro_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
+r2_fm_m = micro_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme1','reversal','mom']).mean()
 print( res_m.summary )
 
-fm_ind = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=sic_df)
+fm_ind = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom", data=sic_df)
 res_ind = fm_ind.fit(cov_type='kernel')
-r2_fm_ind = sic_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
+r2_fm_ind = sic_df.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme1','reversal','mom']).mean()
 print( res_ind.summary )
 
 df_q = pd.read_csv( "Data/all_df_q.csv" )
@@ -78,47 +78,47 @@ df_q = df_q[ ( df_q['jdate']>='1975-07-30') ]
 df_q.set_index( [ 'permno', 'jdate' ], inplace=True )
 df_q[ 'er' ] = df_q[ 'er' ]*100
 
-fm_q = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom", data=df_q)
+fm_q = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom", data=df_q)
 res_q = fm_q.fit(cov_type='kernel')
-r2_fm_q = df_q.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme','reversal','mom']).mean()
+r2_fm_q = df_q.groupby('jdate').apply(fm, var=['GP', 'gat','logbm','logme1','reversal','mom']).mean()
 print(res_q.summary)
 
 # interactions
 interaction_df = all_df
 
-fm_mom = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + mom*logbm", data=interaction_df)
+fm_mom = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom + reversal*mom + mom*logbm", data=interaction_df)
 res_mom = fm_mom.fit(cov_type='kernel')
-r2_fm_mom = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + mom*logbm").mean()
+r2_fm_mom = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme1+reversal+mom + reversal*mom + mom*logbm").mean()
 print(res_mom.summary)
 
-fm_bm = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom+ logbm*mom + logbm*logme", data=interaction_df)
+fm_bm = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom+ logbm*mom + logbm*logme1", data=interaction_df)
 res_bm = fm_bm.fit(cov_type='kernel')
-r2_fm_bm = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom+ logbm*mom + logbm*logme").mean()
+r2_fm_bm = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme1+reversal+mom+ logbm*mom + logbm*logme1").mean()
 print(res_bm.summary)
 
-fm_int = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + logbm*mom + logbm*logme", data=interaction_df)
+fm_int = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom + reversal*mom + logbm*mom + logbm*logme1", data=interaction_df)
 res_int = fm_int.fit(cov_type='kernel')
-r2_fm_int = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + logbm*mom + logbm*logme").mean()
+r2_fm_int = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme1+reversal+mom + reversal*mom + logbm*mom + logbm*logme1").mean()
 print(res_int.summary)
 
 interaction_df = all_df[ (all_df['szport']!='Micro') ]
 
-fm_mom_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + mom*logbm", data=interaction_df)
+fm_mom_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom + reversal*mom + mom*logbm", data=interaction_df)
 res_mom_nomic = fm_mom_nomic.fit(cov_type='kernel')
-r2_fm_mom_nomic = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + mom*logbm").mean()
+r2_fm_mom_nomic = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme1+reversal+mom + reversal*mom + mom*logbm").mean()
 print(res_mom_nomic.summary)
 
-fm_bm_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom+ logbm*mom + logbm*logme", data=interaction_df)
+fm_bm_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom+ logbm*mom + logbm*logme1", data=interaction_df)
 res_bm_nomic = fm_bm_nomic.fit(cov_type='kernel')
-r2_fm_bm_nomic = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom+ logbm*mom + logbm*logme").mean()
+r2_fm_bm_nomic = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme1+reversal+mom+ logbm*mom + logbm*logme1").mean()
 print(res_bm_nomic.summary)
 
-fm_int_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + logbm*mom + logbm*logme", data=interaction_df)
+fm_int_nomic = FamaMacBeth.from_formula( "er ~ 1 + GP+gat+logbm+logme1+reversal+mom + reversal*mom + logbm*mom + logbm*logme1", data=interaction_df)
 res_int_nomic = fm_int_nomic.fit(cov_type='kernel')
-r2_fm_int_nomic = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme+reversal+mom + reversal*mom + logbm*mom + logbm*logme").mean()
+r2_fm_int_nomic = interaction_df.groupby('jdate').apply(fm_iteraction, formula="er ~ 1 + GP+gat+logbm+logme1+reversal+mom + reversal*mom + logbm*mom + logbm*logme1").mean()
 print(res_int_nomic.summary)
 
-stats_all = all_df[ ['GP', 'gat', 'logbm', 'logme', 'reversal', 'mom']].describe( 
+stats_all = all_df[ ['GP', 'gat', 'logbm', 'logme1', 'reversal', 'mom']].describe( 
     percentiles=[ .01, .25, .5, .75, .99 ]).T.round( 3 ).drop('count', axis=1)
 stats_all.round(2).to_latex( 'results/statistics.tex' )
 
